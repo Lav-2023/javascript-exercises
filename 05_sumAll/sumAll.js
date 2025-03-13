a17240efd47bbd6b7b5bddda69f5ec5d746229a1
Lavanya Chalasani <157434431+Lav-2023@users.noreply.github.com>
2025-03-13 11:11:36 -0700

    Updated the code with conditional statements and for loops in the file.

--- 05_sumAll/sumAll.js
+++ 05_sumAll/sumAll.js
@@ -1,5 +1,17 @@
-const sumAll = function() {
-
+const sumAll = function(firstInt, lastInt) {
+    if(firstInt < 0 || lastInt < 0) return "ERROR";
+    if(!Number.isInteger(firstInt) || (!Number.isInteger(lastInt))) return "ERROR";
+    let sumOfInt = 0;
+    if(firstInt > lastInt) {
+        for(let i = lastInt; i <= firstInt; i++) {
+            sumOfInt += i;
+        }
+    } else {
+        for(let i = firstInt; i <= lastInt; i++) {
+            sumOfInt += i;
+        }
+    }
+    return sumOfInt;
 };
 
 // Do not edit below this line
