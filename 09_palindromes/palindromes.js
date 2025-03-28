f83f7fc1171b0bcf07874877dec4ea5195d10991
Lavanya Chalasani <157434431+Lav-2023@users.noreply.github.com>
2025-03-28 14:16:47 -0700

    Added regular expression and several methods to the function such as split, reverse, replace, join, etc.

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = (string) => {
+    let regEx = /[\W_]/g;
+    let replaceStr = string.toLowerCase().replace(regEx, '');
+    let reverseStr = replaceStr.split('').reverse().join('');
+    return reverseStr === replaceStr;
 };
 
 // Do not edit below this line
