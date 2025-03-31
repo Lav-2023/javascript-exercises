26d39fea6b2cb270fbf437c1fc465050dc69b060
Lavanya Chalasani <157434431+Lav-2023@users.noreply.github.com>
2025-03-31 16:06:36 -0700

    Added map method to the function.

--- 11_getTheTitles/getTheTitles.js
+++ 11_getTheTitles/getTheTitles.js
@@ -1,6 +1,18 @@
-const getTheTitles = function() {
+const getTheTitles = function(array) {
+    const books = [
+        {
+          title: 'Book',
+          author: 'Name'
+        },
+        {
+          title: 'Book2',
+          author: 'Name2'
+        }
+      ]
 
+    return array.map(books => books.title);
 };
+
 
 // Do not edit below this line
 module.exports = getTheTitles;
