932a2cb9d5d051f200e663c19df218e305b97ff6
Lavanya Chalasani <157434431+Lav-2023@users.noreply.github.com>
2025-04-01 09:30:58 -0700

    Added Date function and built-in array methods along with conditional statements to the function.

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,16 @@
-const findTheOldest = function() {
+const age = (birthYear, deathYear) => {
+    if(!deathYear) {
+        deathYear = new Date().getFullYear()
+    }
+    return deathYear - birthYear;
+}
 
+const findTheOldest = (people) => {
+    return people.reduce((oldest, currentPerson) => {
+        const oldestAge = age(oldest.yearOfBirth, oldest.yearOfDeath);
+        const currentAge = age(currentPerson.yearOfBirth, currentPerson.yearOfDeath);
+        return oldestAge < currentAge ? currentPerson : oldest;
+    })
 };
 
 // Do not edit below this line
